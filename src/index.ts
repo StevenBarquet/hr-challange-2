0a20a07c46f21fc6b564eedec3669961dea049dc
Steven Barquet <mlmjm123@gmail.com>
2021-12-16 09:42:53 -0600

    challange 2 solved

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 /* eslint-disable no-return-assign */
 
-// Source: https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
+// Source: https://www.hackerrank.com/challenges/a-very-big-sum/problem?isFullScreen=true
 
-function simpleArraySum(ar: Array<number>): number {
-  let suma = 0;
-  ar.forEach((element) => suma += element);
-  return suma;
+function aVeryBigSum(ar: number[]): number {
+  // Write your code here
+  let sum = 0;
+  ar.forEach( num => sum= sum + num);
+  return sum;
 }
 
 function main() {
   console.log('---------- Cool Programing ----------\n');
   const testArray = [1, 2, 3, 4, 10, 11];
-  const result = simpleArraySum(testArray);
+  const result = aVeryBigSum(testArray);
   console.log('Result: ', result);
 }
 
